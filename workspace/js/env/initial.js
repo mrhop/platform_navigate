756fd107291a78e6c81c500cfa24910106101020
mrhop <233402923@qq.com>
2016-09-14 10:01:11 +0800

    init platform_navigate

--- workspace/js/env/initial.js
+++ workspace/js/env/initial.js
@@ -2,7 +2,21 @@
  * Created by Donghui Huo on 2016/4/26.
  */
 ReactIntl.addLocaleData([...ReactIntlEn, ...ReactIntlZh]);
+var localeLanguage = 'en-US';
+if (navigator.browserLanguage) {
+    if (navigator.browserLanguage.toLowerCase() == 'zh-cn') {
+        localeLanguage = 'zh-CN';
+    } else {
+        //localeLanguage = navigator.systemLanguage
+    }
+} else {
+    if (navigator.language.toLowerCase() == 'zh-cn') {
+        localeLanguage = 'zh-CN';
+    } else {
+        //localeLanguage = navigator.language
+    }
+}
 module.exports = {
     //locale:'en-US'
-    locale:'zh-CN'
+    locale: localeLanguage
 }
